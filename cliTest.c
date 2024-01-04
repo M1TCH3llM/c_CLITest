c312ee4e85e9f2aa7e0cf7ea8e478ffbc3037cad
Mitchell.Morgan <mitchemorgan@gmail.com>
2024-01-03 21:06:40 -0700

    added final tally

--- cliTest.c
+++ cliTest.c
@@ -47,6 +47,7 @@ int main(void)
         }
 
         printf("Question 3\n");
+        // get_int used to retrieve input intergers
         int answer3 = get_int("How many countries are in the UN? ");
 
         if (answer3 == 193)
@@ -59,8 +60,32 @@ int main(void)
             printf("Incorrect, Score: %d\n", score);
         }
 
+        printf("Question 4\n");
+        // get_int used to retrieve input intergers
+        int answer4 = get_char("What fraction is higher\n a. 45/50\n b. 112/150\n c. 235/300?\n ");
+
+        if (answer4 == 'a')
+        {
+            score++;
+            printf("Correct, Score: %d\n", score);
+        } 
+        else 
+        {
+            printf("Incorrect, Score: %d\n", score);
+        }
+    
 
     }
+    
+    if (score == 4) {
+        printf("HIGH SCORE!!!! ");
+    } else if (score >= 2)
+    {
+        printf("Nice Try");
+    } else {
+        printf("Better Luck Next Time");
+    }
+    printf("Your Score: %d\n", score);
 
     return 0;
 }
